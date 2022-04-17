844a29a0c392599a0fb2f4db1b4386e6d02230f0
Nagaraj N <nnagarajece31@gmail.com>
2022-04-17 21:02:37 +0530

    ui changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import Page2Index from './page2'
 
 const Home: NextPage = () => {
   return (
     <body>
-      <h1 className={'text-3xl,font-bold,underline'}>Hello world!</h1>
+      <h1>Hello Next JS</h1>
+      <br />
+      <Link href="/page2">
+        <a className="rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+          Navigate
+        </a>
+      </Link>
     </body>
   )
 }
---
+++ pages/page2/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Page2Index: NextPage = () => {
+  return (
+    <body>
+      <h1 className={'text-3xl,font-bold,underline'}>Page2</h1>
+    </body>
+  )
+}
+
+export default Page2Index
